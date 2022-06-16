93db6e7ad6e1dbfc949b6bb8313379eceff1da6c
Vesper <3692034+noodlecollie@users.noreply.github.com>
2022-06-16 21:13:08 +0100

    Added dimension check to terrain layers

--- gamelib/src/descriptor/terraindescriptor.c
+++ gamelib/src/descriptor/terraindescriptor.c
@@ -3,6 +3,7 @@
 #include "gamelib/external/raylibheaders.h"
 #include "gamelib/gametypes.h"
 #include "gamelib/drawinglayers.h"
+#include "gamelib/stringutil.h"
 #include "external/cJSON_wrapper.h"
 #include "external/cJSON_util.h"
 #include "resourcepool/resourcepool.h"
@@ -12,6 +13,7 @@
 
 typedef struct TerrainLayer
 {
+	char* name;
 	ResourcePoolTexture* imageResource;
 } TerrainLayer;
 
@@ -19,10 +21,22 @@ struct TerrainDescriptor
 {
 	TerrainLayer* layers;
 	DrawingLayer defaultDrawingLayer;
+	Vector2i dimensions;
 };
 
-static void DestroyLayer(TerrainLayer* layer)
+static inline bool DimensionsAreValid(const TerrainDescriptor* descriptor)
 {
+	return descriptor->dimensions.x > 0 && descriptor->dimensions.y > 0;
+}
+
+static void DeallocateLayer(TerrainLayer* layer)
+{
+	if ( layer->name )
+	{
+		MemFree(layer->name);
+		layer->name = NULL;
+	}
+
 	if ( layer->imageResource )
 	{
 		ResourcePool_RemoveTextureRef(layer->imageResource);
@@ -30,62 +44,126 @@ static void DestroyLayer(TerrainLayer* layer)
 	}
 }
 
-static void LoadLayer(const char* filePath, cJSON* content, TerrainDescriptor* descriptor)
+static bool CheckLayerImageDimensions(const char* filePath, TerrainDescriptor* descriptor, TerrainLayer* layerEntry)
 {
-	cJSON* nameItem = cJSONWrapper_GetObjectItemOfType(content, "name", cJSON_String);
-	const char* name = nameItem ? nameItem->valuestring : NULL;
+	Texture2D* tex = ResourcePool_GetTexture(layerEntry->imageResource);
 
-	cJSON* imageItem = cJSONWrapper_GetObjectItemOfType(content, "image", cJSON_String);
-
-	if ( !imageItem || !imageItem->valuestring || !imageItem->valuestring[0] )
+	if ( !DimensionsAreValid(descriptor) )
 	{
-		TraceLog(LOG_WARNING, "TERRAIN DESCRIPTOR: [%s] Terrain layer \"%s\" had no image specified, skipping.", filePath, name ? name : "unnamed");
-		return;
-	}
+		descriptor->dimensions.x = tex->width;
+		descriptor->dimensions.y = tex->height;
 
-	cJSON* layerIndexItem = cJSONWrapper_GetObjectItemOfType(content, "terrain_layer_index", cJSON_Number);
-
-	if ( !layerIndexItem )
-	{
-		TraceLog(LOG_WARNING, "TERRAIN DESCRIPTOR: [%s] Terrain layer \"%s\" had no layer index specified, skipping.", filePath, name ? name : "unnamed");
-		return;
-	}
-
-	int layerIndex = layerIndexItem->valueint;
-
-	if ( layerIndex < 0 || layerIndex >= (int)TERRAIN_MAX_LAYERS )
-	{
 		TraceLog(
-			LOG_WARNING,
-			"TERRAIN DESCRIPTOR: [%s] Terrain layer \"%s\" specified layer index %d out of valid range [%d %d], skipping.",
+			LOG_DEBUG,
+			"TERRAIN DESCRIPTOR: [%s] Dimensions of terrain set by first encountered layer: %dx%d",
 			filePath,
-			name ? name : "unnamed",
-			layerIndex,
-			0,
-			TERRAIN_MAX_LAYERS - 1
+			descriptor->dimensions.x,
+			descriptor->dimensions.y
 		);
 
-		return;
+		return true;
 	}
 
-	TerrainLayer* layerEntry = &descriptor->layers[layerIndex];
-
-	layerEntry->imageResource = ResourcePool_LoadTextureAndAddRef(imageItem->valuestring);
-
-	if ( !layerEntry->imageResource )
+	if ( tex->width != descriptor->dimensions.x || tex->height != descriptor->dimensions.y )
 	{
 		TraceLog(
 			LOG_ERROR,
-			"TERRAIN DESCRIPTOR: [%s] Terrain layer \"%s\" could not load image %s.",
+			"TERRAIN DESCRIPTOR: [%s] Terrain layer \"%s\" dimensions of %dx%d did not match terrain dimensions of %dx%d, not loading.",
 			filePath,
-			name ? name : "unnamed",
-			imageItem->valuestring
+			layerEntry->name ? layerEntry->name : "unnamed",
+			tex->width,
+			tex->height,
+			descriptor->dimensions.x,
+			descriptor->dimensions.y
 		);
 
-		return;
+		return false;
 	}
 
-	TraceLog(LOG_DEBUG, "TERRAIN DESCRIPTOR: [%s] Successfully loaded terrain layer \"%s\"", filePath, name ? name : "unnamed");
+	return true;
+}
+
+static void LoadLayer(const char* filePath, cJSON* content, TerrainDescriptor* descriptor)
+{
+	TerrainLayer* layerEntry = NULL;
+
+	do
+	{
+		cJSON* nameItem = cJSONWrapper_GetObjectItemOfType(content, "name", cJSON_String);
+		cJSON* imageItem = cJSONWrapper_GetObjectItemOfType(content, "image", cJSON_String);
+		const char* name = nameItem ? nameItem->valuestring : NULL;
+		const char* logName = name ? name : "unnamed";
+
+		if ( !imageItem || !imageItem->valuestring || !imageItem->valuestring[0] )
+		{
+			TraceLog(LOG_WARNING, "TERRAIN DESCRIPTOR: [%s] Terrain layer \"%s\" had no image specified, skipping.", filePath, logName);
+			break;
+		}
+
+		cJSON* layerIndexItem = cJSONWrapper_GetObjectItemOfType(content, "terrain_layer_index", cJSON_Number);
+
+		if ( !layerIndexItem )
+		{
+			TraceLog(LOG_WARNING, "TERRAIN DESCRIPTOR: [%s] Terrain layer \"%s\" had no layer index specified, skipping.", filePath, logName);
+			break;
+		}
+
+		int layerIndex = layerIndexItem->valueint;
+
+		if ( layerIndex < 0 || layerIndex >= (int)TERRAIN_MAX_LAYERS )
+		{
+			TraceLog(
+				LOG_WARNING,
+				"TERRAIN DESCRIPTOR: [%s] Terrain layer \"%s\" specified layer index %d out of valid range [%d %d], skipping.",
+				filePath,
+				logName,
+				layerIndex,
+				0,
+				TERRAIN_MAX_LAYERS - 1
+			);
+
+			break;
+		}
+
+		layerEntry = &descriptor->layers[layerIndex];
+
+		if ( name && *name )
+		{
+			layerEntry->name = DuplicateString(name);
+		}
+
+		layerEntry->imageResource = ResourcePool_LoadTextureAndAddRef(imageItem->valuestring);
+
+		if ( !layerEntry->imageResource )
+		{
+			TraceLog(
+				LOG_ERROR,
+				"TERRAIN DESCRIPTOR: [%s] Terrain layer \"%s\" could not load image %s.",
+				filePath,
+				logName,
+				imageItem->valuestring
+			);
+
+			break;
+		}
+
+		if ( !CheckLayerImageDimensions(filePath, descriptor, layerEntry) )
+		{
+			break;
+		}
+
+		TraceLog(LOG_DEBUG, "TERRAIN DESCRIPTOR: [%s] Successfully loaded terrain layer \"%s\"", filePath, logName);
+
+		// Success
+		return;
+	}
+	while ( false );
+
+	// Something went wrong.
+	if ( layerEntry )
+	{
+		DeallocateLayer(layerEntry);
+	};
 }
 
 static void LoadLayers(const char* filePath, cJSON* content, TerrainDescriptor* descriptor)
@@ -172,7 +250,7 @@ void TerrainDescriptor_Destroy(TerrainDescriptor* descriptor)
 	{
 		for ( size_t index = 0; index < TERRAIN_MAX_LAYERS; ++index )
 		{
-			DestroyLayer(&descriptor->layers[index]);
+			DeallocateLayer(&descriptor->layers[index]);
 		}
 
 		MemFree(descriptor->layers);
