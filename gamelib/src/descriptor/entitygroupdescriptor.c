e1652c424226406caa79002b3c96281748f20aa1
Vesper <3692034+noodlecollie@users.noreply.github.com>
2022-06-29 08:25:49 +0100

    Beginnings of entity group loading

--- gamelib/src/descriptor/entitygroupdescriptor.c
+++ gamelib/src/descriptor/entitygroupdescriptor.c
@@ -1,15 +1,94 @@
 #include <stddef.h>
 #include "descriptor/entitygroupdescriptor.h"
 #include "gamelib/external/raylibheaders.h"
+#include "gamelib/stringutil.h"
 #include "external/cJSON_wrapper.h"
 #include "descriptor/descriptorutil.h"
 
 #define SUPPORTED_VERSION 1
 
+typedef enum
+{
+	ITEMTYPE_SPRITESHEET,
+	ITEMTYPE_TERRAIN
+} ItemType;
+
+struct EntityGroupItem
+{
+	EntityGroupItem* prev;
+	EntityGroupItem* next;
+	ItemType itemType;
+	void* item;
+};
+
 struct EntityGroupDescriptor
 {
-	int dummy;
+	char* name;
+	EntityGroupItem* head;
+	EntityGroupItem* tail;
 };
+
+static void DestroyItem(EntityGroupItem* item)
+{
+	MemFree(item);
+}
+
+static void LoadEntity(const char* filePath, cJSON* ent, EntityGroupDescriptor* descriptor)
+{
+	// TODO
+	(void)filePath;
+	(void)ent;
+	(void)descriptor;
+}
+
+static void LoadEntities(const char* filePath, cJSON* content, EntityGroupDescriptor* descriptor)
+{
+	cJSON* entities = cJSONWrapper_GetObjectItemOfType(content, "entities", cJSON_Array);
+
+	if ( !entities )
+	{
+		return;
+	}
+
+	for ( cJSON* child = entities->child; child; child = child->next )
+	{
+		LoadEntity(filePath, child, descriptor);
+	}
+}
+
+static EntityGroupDescriptor* Load(const char* filePath, cJSON* root)
+{
+	EntityGroupDescriptor* descriptor = NULL;
+	cJSON* content = Descriptor_GetContent(root, "entitygroup", SUPPORTED_VERSION);
+
+	do
+	{
+		if ( content )
+		{
+			cJSON* nameField = cJSONWrapper_GetObjectItemOfType(content, "name", cJSON_String);
+
+			if ( !nameField || !nameField->valuestring || !(*nameField->valuestring) )
+			{
+				TraceLog(LOG_ERROR, "ENTITY GROUP DESCRIPTOR: [%s] No name was specified for entity group", filePath);
+				break;
+			}
+
+			descriptor = (EntityGroupDescriptor*)MemAlloc(sizeof(EntityGroupDescriptor));
+			descriptor->name = DuplicateString(nameField->valuestring);
+
+			LoadEntities(filePath, content, descriptor);
+
+			TraceLog(LOG_INFO, "ENTITY GROUP DESCRIPTOR: [%s] Loaded successfully", filePath);
+		}
+		else
+		{
+			TraceLog(LOG_ERROR, "ENTITY GROUP DESCRIPTOR: [%s] Did not describe a version %d entity group", filePath, SUPPORTED_VERSION);
+		}
+	}
+	while ( false );
+
+	return descriptor;
+}
 
 EntityGroupDescriptor* EntityGroupDescriptor_LoadFromJSONFile(const char* filePath)
 {
@@ -26,22 +105,20 @@ EntityGroupDescriptor* EntityGroupDescriptor_LoadFromJSONFile(const char* filePa
 		return NULL;
 	}
 
-	EntityGroupDescriptor* descriptor = NULL;
-	cJSON* content = Descriptor_GetContent(root, "entitygroup", SUPPORTED_VERSION);
-
-	if ( content )
-	{
-		descriptor = (EntityGroupDescriptor*)MemAlloc(sizeof(EntityGroupDescriptor));
-
-		TraceLog(LOG_INFO, "ENTITY GROUP DESCRIPTOR: [%s] Loaded successfully", filePath);
-	}
-	else
-	{
-		TraceLog(LOG_ERROR, "ENTITY GROUP DESCRIPTOR: [%s] Did not describe a version %d entity group", filePath, SUPPORTED_VERSION);
-	}
+	EntityGroupDescriptor* descriptor = Load(filePath, root);
 
 	cJSON_Delete(root);
 	return descriptor;
+}
+
+EntityGroupDescriptor* EntityGroupDescriptor_LoadFromJSONObject(cJSON* root)
+{
+	if ( !root )
+	{
+		return NULL;
+	}
+
+	return Load("local object", root);
 }
 
 void EntityGroupDescriptor_Destroy(EntityGroupDescriptor* descriptor)
@@ -49,6 +126,24 @@ void EntityGroupDescriptor_Destroy(EntityGroupDescriptor* descriptor)
 	if ( !descriptor )
 	{
 		return;
+	}
+
+	if ( descriptor->name )
+	{
+		MemFree(descriptor->name);
+	}
+
+	EntityGroupItem* item = descriptor->head;
+	descriptor->head = NULL;
+	descriptor->tail = NULL;
+
+	while ( item )
+	{
+		EntityGroupItem* next = item->next;
+
+		DestroyItem(item);
+
+		item = next;
 	}
 
 	MemFree(descriptor);
--- gamelib/src/descriptor/entitygroupdescriptor.h
+++ gamelib/src/descriptor/entitygroupdescriptor.h
@@ -1,6 +1,10 @@
 #pragma once
 
+#include "external/cJSON_wrapper.h"
+
 typedef struct EntityGroupDescriptor EntityGroupDescriptor;
+typedef struct EntityGroupItem EntityGroupItem;
 
 EntityGroupDescriptor* EntityGroupDescriptor_LoadFromJSONFile(const char* filePath);
+EntityGroupDescriptor* EntityGroupDescriptor_LoadFromJSONObject(cJSON* root);
 void EntityGroupDescriptor_Destroy(EntityGroupDescriptor* descriptor);
